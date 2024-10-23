581f1049c6e1d111799a2ef785dbc7b0e7985a67
JoeBannana <102961244+JoeBannana@users.noreply.github.com>
2024-10-23 11:27:48 +0000

    13>27 kori alapot

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
-
-st.title("🎈 My new app")
+import pandas as pd
+import altair as alt
+st.set_page_config(page_title="Analýza dataset Filmy", page_icon="🎬")
+st.title("🎬 Analýza dataset Filmy")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    """
+    Táto aplikácia vizualizuje údaje z [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
+    Ukazuje, ktorý filmový žáner mal v kinách za posledné roky najlepší výkon. 
+    Stačí kliknúť na miniaplikácie nižšie a preskúmať ich!
+    """
 )
+
+
